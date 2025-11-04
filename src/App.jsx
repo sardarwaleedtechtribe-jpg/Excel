799d477e888f4bfec73794b6b4ad3f9bb4a8d6de
Waleed-070 <w56772486@gmail.com>
2025-11-04 15:45:13 +0500

    pushing the first time

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Excel from "./Pages/excel.jsx";
+
+function App() {
+  return (
+    <div>
+      <Excel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Excel.jsx
@@ -0,0 +1,9 @@
+
+function Excel() {
+  return (
+    <div>
+      <h1>Excel Page</h1>
+    </div>
+  );
+}
+export default Excel;
